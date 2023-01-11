5e62333bf0d22af65d23032a04145b8c1e831fb4
Nick Mason <nickmason372@yahoo.com>
2023-01-10 23:01:07 -0500

    header started

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './header.css'
+
+const Header = () => {
+    return (
+        <div className="jumbotron">
+        <div className="header-info">
+          <h1 className="greeting"></h1>
+          <p className=" header-text lead font-koulen text-4xl">Nick Mason</p>
+        </div>
+      </div>
+        
+    )
+}
+export default Header
--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap'
 import './index.css';
 import App from './App';
+import Header from './components/Header/Header';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <section>
+      <Header />
+    </section>
     <App />
   </React.StrictMode>
 );
